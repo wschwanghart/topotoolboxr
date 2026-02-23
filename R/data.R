e9f12fd46bfb781887d97ad4d6f05afdad42e71b
Kilian Lenz <132825721+ktlenz@users.noreply.github.com>
2026-02-23 09:25:31 +0100

    Changed path to StackedSpatRaster in .rda (#64)
    Closes #63
    
    Replaces the hard-coded path previously stored in srtm_bigtujunga30m_utm11.rda with a StackedSpatRaster object. The new data should now work across systems.

--- R/data.R
+++ R/data.R
@@ -1,19 +1,17 @@
-#' Path to 30 m SRTM elevation data for Big Tujunga catchment
+#' 30 m SRTM DEM for Big Tujunga catchment
 #'
-#' Character string containing the file path to the 30 m resolution Shuttle Radar 
-#' Topography Mission (SRTM) elevation data TIFF file for the Big Tujunga 
-#' catchment in California, located in `inst/extdata/`.
+#' Wrapped SpatRaster of 30 m SRTM elevation data (643 × 1197 cells, 1 layer,
+#' UTM 11N EPSG:32611) for Big Tujunga, California.
 #'
-#' @format ## `srtm_bigtujunga30m_utm11`
-#' A single length character string giving the full path to the GEOTIFF file:
-#' \describe{
-#'   \item{file}{Path to `srtm_bigtujunga30m_utm11.tif` (643 rows, 1197 columns, 1 layer, WGS 84 / UTM zone 11N EPSG:32611)}
-#' }
-#' @usage data(srtm_bigtujunga30m_utm11)
+#' @format srtm_bigtujunga30m_utm11
+#' Packed SpatRaster list created by terra::wrap().
+#'
 #' @examples
-#' \dontrun{
-#' data(srtm_bigtujunga30m_utm11)
 #' library(topotoolboxr)
-#' GRIDobj(srtm_bigtujunga30m_utm11)
-#' }
+#' data(srtm_bigtujunga30m_utm11)
+#' dem <- rast(srtm_bigtujunga30m_utm11) # unwrap
+#' plot(dem)
+#'
+#' @source USGS SRTM
+#' @export
 "srtm_bigtujunga30m_utm11"
